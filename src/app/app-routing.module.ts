0d28a0036295acd77e8484cabcb2ce9d9ca382b2
Hatem GRIBI <hatemgribi@gmail.com>
2022-09-15 11:08:59 +0200

    Step five done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { MessagesComponent } from './messages/messages.component';
 
 const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
-  { path: 'messages', component: MessagesComponent }
+  { path: 'detail/:id', component: HeroDetailComponent}
 ];
 
 @NgModule({
